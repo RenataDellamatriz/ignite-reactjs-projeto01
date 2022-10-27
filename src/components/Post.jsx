c239933f5f9097004fc27a557fa4fa7cb6dde9f7
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-10-27 16:00:53 -0300

    Handling useState hook

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,64 +1,82 @@
-import { format, formatDistanceToNow } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
-import { LineSegment } from 'phosphor-react';
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-
 //em vez de usar props.authorInfo, usar destructuring
-export function Post({author, publishedAt, content}) {
+export function Post({ author, publishedAt, content }) {
 
-  const publishedDateFormatted = format( publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-    locale: ptBR,
-  });
+  const [comments, setComments] = useState([1, 2]);
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-    locale:ptBR,
-    addSuffix:true,
-  })
+    locale: ptBR,
+    addSuffix: true,
+  });
 
-   return (
+  function handleCreateNewComment() {
+    event.preventDefault(); //previne o comportamente default do html de redirecionar a pessoa para outro lugar
+
+    setComments([...comments, comments.length + 1])
+  }
+  
+
+  return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-            <Avatar src={author.avatarUrl}  />
-            <div className={styles.authorInfo}>
-                <strong>{author.name}</strong>
-                <span>{author.role}</span>
-            </div>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        {content.map( item => {
-          if(item.type=== 'paragraph') {
-            return <p>{item.content}</p>
-          } else if (item.type ==='link') {
-            return <p><a href='#'>{item.content}</a></p>
+        {content.map((item) => {
+          if (item.type === "paragraph") {
+            return <p>{item.content}</p>;
+          } else if (item.type === "link") {
+            return (
+              <p>
+                <a href="#">{item.content}</a>
+              </p>
+            );
           }
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder='Deixe um comentário'/>
-        
+        <textarea placeholder="Deixe um comentário" />
+
         <footer>
-        <button type="submit">Publicar</button>
+          <button type="submit">Publicar</button>
         </footer>
       </form>
-      <div className={styles.commentList}>
 
-      <Comment/>
-      <Comment/>
-      <Comment/>
-      
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
